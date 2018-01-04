876d7e6ed1ac23b5f2f4098b590875421b59c5b3
LungiO <34140837+LungiO@users.noreply.github.com>
2018-01-04 20:57:12 +0100

    CTCI: Add implementation of exercise 16.1

---
+++ src/main/java/ctci/chapter_16/Exercise_16_1.java
@@ -0,0 +1,14 @@
+package ctci.chapter_16;
+
+/**
+ * 16.1 Number Swapper:
+ * Write a function to swap a number in place (that is, without temporary variables).
+ */
+
+class Exercise_16_1 {
+    void solution(SwapNumberPair pair) {
+        pair.num1 ^= pair.num2;
+        pair.num2 ^= pair.num1;
+        pair.num1 ^= pair.num2;
+    }
+}
---
+++ src/main/java/ctci/chapter_16/SwapNumberPair.java
@@ -0,0 +1,6 @@
+package ctci.chapter_16;
+
+class SwapNumberPair {
+    int num1;
+    int num2;
+}
